dc8a4937219cd5b9d071c04559f29fe4d6e1025c
fernandomartin04 <fernandomartin04@iesamachado.org>
2023-11-08 00:55:46 +0100

    revision 1

---
+++ Javascript/revisiones/revision1-1.js
@@ -0,0 +1,24 @@
+        function btnrojo(){
+            if(document.getElementById("rojo").style.visibility == "hidden"){
+                document.getElementById("rojo").style.visibility = "visible";
+            }
+            else{
+                document.getElementById("rojo").style.visibility = "hidden";
+            }
+        }
+        function btnverde(){
+            if(document.getElementById("verde").style.visibility == "hidden"){
+                document.getElementById("verde").style.visibility = "visible";
+            }
+            else{
+                document.getElementById("verde").style.visibility = "hidden";
+            }
+        }
+        function btnazul(){
+            if(document.getElementById("azul").style.visibility == "hidden"){
+                document.getElementById("azul").style.visibility = "visible";
+            }
+            else{
+                document.getElementById("azul").style.visibility = "hidden";
+            }       
+        }

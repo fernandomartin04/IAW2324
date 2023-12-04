4c155c2dbb132ac850b918b92911e830a9f77a1a
fernandomartin04 <fernandomartin04@gmail.com>
2023-12-04 14:20:56 +0100

    encuesta

--- php/encuesta.php
+++ php/encuesta.php
@@ -7,10 +7,18 @@
 </head>
 <body>
     <form action="" method="post">
-        Nombre: <input type="text" name="nombre"><br>
-        Apellido: <input type="text" name="apellido"><br>
-        Pelicula favorita: <input type="text" name="pelicula"><br>
-        Eres mayor de edad: <input type="checkbox" name="edad">
+        Nombre: <input type="text" name="nombre"><br><br>
+        Apellido: <input type="text" name="apellido"><br><br>
+        Pelicula favorita: <input type="text" name="pelicula"><br><br>
+        Eres mayor de edad: <input type="checkbox" name="edad"><br><br>
+        Equipo favorito: <br><br>
+        <input type="radio" name="futbol" value="sevilla"> Sevilla:
+        <input type="radio" name="futbol" value="barsa"> Barsa:
+        <input type="radio" name="futbol" value="madrid"> Madrid:
+        <input type="radio" name="futbol" value="betis"> Betih
+
+
+
     </form>
 
     <?php
@@ -19,7 +27,16 @@
         $apellido = htmlspecialchars($_POST['apellido']);
         $pelicula = htmlspecialchars($_POST['pelicula']);
         $edad = htmlspecialchars($_POST['edad']);
+        $sevilla = htmlspecialchars($_POST['futbol']);
 
+        if(isset($_POST["submit"])) {
+            if ($checkbox =='true' && $contrasena=='FernandoElH4acker') {
+                echo "<script>alert('Acceso Aceptado');</script>";
+            }
+            else {
+                echo "<script>alert('Acceso Denegado');</script>";
+            }
+        }
 
     ?>
 

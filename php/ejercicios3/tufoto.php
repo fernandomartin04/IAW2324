9fccf5fa417fab59039d28d8767783e5eb676e45
fernandomartin04 <fernandomartin04@iesamachado.org>
2023-12-05 00:55:03 +0100

    tufoto

--- php/ejercicios3/tufoto.php
+++ php/ejercicios3/tufoto.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tu foto</title>
 </head>
 <body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+
+        <input type="text" name="nombre"><br><br>
+        <input type="file" name="file"><br><br>
+        <input type="submit" name="submit">
+    </form>
+    <?php
+        $nombre = $archivo = $submit = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = test_input($_POST["nombre"]);
+            $archivo = test_input($_FILES["file"]);
+            $submit = test_input($_POST["submit"]);
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+          }
+
+        if(isset($_POST["submit"])) {
+            if (empty($file)) {
+                echo $nombre;
+            }
+            else {
+                echo "<br>" . $nombre . "<br>" . $archivo;
+            }
+        }
+
+    ?>
+
     
 </body>
 </html>

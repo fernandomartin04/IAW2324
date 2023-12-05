d4909b2606670887c4864b45b0541e08a3af8f58
fernandomartin04 <fernandomartin04@gmail.com>
2023-12-05 14:23:29 +0100

    sorteo

---
+++ php/ejercicios3/sorteo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteo en php</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+        <input type="text" name="numero" placeholder="Escribe un número de participantes"><br><br>
+        <input type="submit" name="submit">
+    </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $numero = htmlspecialchars($_POST["numero"]);
+            $submit = htmlspecialchars($_POST["submit"]);
+            //if(!is_numeric($numero)){
+             //   echo "<p>El valor deb de ser un número</p>"
+           // }
+           // else {
+                echo rand(1,$numero);
+           // }
+        }
+    ?>
+</body>
+</html>

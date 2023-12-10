dceb14a335d83263ab5d0f6728476fd0cdddb6f8
fernandomartin04 <fernandomartin04@iesamachado.org>
2023-12-10 22:16:17 +0100

    head

--- php/ejercicios4/header.php
+++ php/ejercicios4/header.php
@@ -7,9 +7,16 @@
 </head>
 <body>
     
+    <p>Haz click para irte a la página de hola.php</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <input type="submit" value="voy a holaphp">
+    </form>
     <?php 
-        header("Location: hola.php");
-        exit();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            header("Location: hola.php");
+            exit();
+        }
+            
     ?>
 
 </body>

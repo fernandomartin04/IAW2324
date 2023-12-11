122eea5b2851c2c10c6c6880fe8bd66a5b40cc44
fernandomartin04 <fernandomartin04@iesamachado.org>
2023-12-11 20:10:50 +0100

    aeg

--- php/simulacro-php1/habitacion.php
+++ php/simulacro-php1/habitacion.php
@@ -12,28 +12,47 @@
         <input type="text" name="email" id="email" placeholder="Escribe tu email"><br><br>
         <input type="text" name="dni" id="dni" placeholder="Escribe tu dni"><br><br>
         <label for="habitaciones">Elige el tipo de habitación: </label>
-        <select name="cars" id="cars">
-            <option value="simple">Simple: 65€</option>
-            <option value="doble">Doble: 80€</option>
-            <option value="triple">Triple: 140€</option>
-            <option value="suite">Suite: 180€</option>
+        <select name="habitacion" id="habitacion">
+            <option value="simple" name="simple">Simple: 65€</option>
+            <option value="doble" name="doble">Doble: 80€</option>
+            <option value="triple" name="triple">Triple: 140€</option>
+            <option value="suite" name="suite">Suite: 180€</option>
         </select>
         <input type="submit" name="submit" id="submit">
     </form>
 
     <?php
         //Función que valida el dni
-        function validacion_dni($dni){ 
+        function validacionDni($dni){ 
             $misletras = substr($dni, -1);
             $numerosdni = substr($dni, 0, -1);
             return (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numerosdni%23, 1) == $misletras && strlen($misletras) == 1 && strlen ($numerosdni) == 8);
         }
 
         //Función que valida el correo
-        function validateEmail($email) {
+        function validacionEmail($email) {
             return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
         }
 
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = htmlspecialchars($_POST["nombre"]);
+            $apellido = htmlspecialchars($_POST["apellido"]);
+            $email = htmlspecialchars($_POST["email"]);
+            $dni = htmlspecialchars($_POST["dni"]);
+            $submit = htmlspecialchars($_POST["submit"]);
+
+            if ($habitacion != "" && $email != "" && $dni != "" && $nombre != "" && $apellido != "") {
+
+                //Si todos los campos están rellenos, ejecuto las funciones de validacion
+                $dniValido = validacionDni($dni);
+                $emailValido = validacionEmail($email);
+            }
+
+            else {
+                echo "<p>Todo el formulario hay que responderlo</p>"
+            }
+        }
         
     ?>
 

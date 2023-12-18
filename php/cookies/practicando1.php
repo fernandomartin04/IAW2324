337c384dfce70d08b70b0c215a3b0dc87c3ff5b0
fernandomartin04 <fernandomartin04@gmail.com>
2023-12-18 14:25:07 +0100

    v

---
+++ php/cookies/practicando1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicando con cookies</title>
+</head>
+<body>
+    <?php
+        setcookie("nombre", "Pepito");
+    ?>
+</body>
+</html>

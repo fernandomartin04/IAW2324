b9606a28a3802569ca57d1bcffcc4309567abd7c
fernandomartin04 <fernandomartin04@gmail.com>
2024-01-09 14:27:26 +0100

    sql

---
+++ php/2TRIMESTRE/MYSQL/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "loading.thsite.top";
+$username = "thsi_35754745";
+$password = "nBogJY9x";
+$dbname = "thsi_35754745_proyecto";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?> 

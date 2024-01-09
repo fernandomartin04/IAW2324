63c41fe139ff42216dfdae3ec193a060358451be
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-01-09 19:55:43 +0100

    conn

--- php/2TRIMESTRE/MYSQL/conexion.php
+++ php/2TRIMESTRE/MYSQL/conexion.php
@@ -5,7 +5,7 @@ $password = "nBogJY9x";
 $dbname = "thsi_35754745_proyecto";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername; $dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";

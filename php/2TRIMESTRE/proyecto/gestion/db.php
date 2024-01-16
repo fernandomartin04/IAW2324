b17abaf0327d8db852bbb0927fac1fa5b7bd0044
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-01-16 16:48:56 +0100

    ok

--- php/2TRIMESTRE/proyecto/gestion/db.php
+++ php/2TRIMESTRE/proyecto/gestion/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'sdb-w.hosting.stackcp.net';   
-$user = 'gestion_incidencias-323133eda3';   
-$pass = "en97j64z81";   
-$database = 'gestion_incidencias-323133eda3';     
-$conn = mysqli_connect($host,$user,$pass,$database);   
-if (!$conn) {                                             
-    die("Conexión fallida con base de datos: " . mysqli_connect_error());     
-  }
+$servername = 'sql307.thsite.top';   
+$username = 'thsi_35748541';   
+$password = "GkIoSmUV";   
+$dbname = 'thsi_35748541_proyecto';     
+$conn = new mysqli($servername, $username, $password, $dbname);   
+if ($conn->connect_error) {                                             
+    die("Conexión fallida con base de datos: " . $conn->connect_error);     
+} echo "Conectado con exito a la base de datos <br>"
 ?>
 
 
